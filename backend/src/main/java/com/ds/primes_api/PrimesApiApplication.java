14fa6b31c7f8f28ba79aa81bd83b1ec9f63e21e9
Isaí Quiroga <isai.quiroga.2004@gmail.com>
2024-10-02 06:38:45 -0500

    Reorganización de directorios

---
+++ backend/src/main/java/com/ds/primes_api/PrimesApiApplication.java
@@ -0,0 +1,13 @@
+package com.ds.primes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimesApiApplication.class, args);
+	}
+
+}
